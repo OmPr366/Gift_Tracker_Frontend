d313fcc7b48ebfbdf7219da9aeea93ac4ad63879
Om Prakash <ompra.rox@gmail.com>
2023-04-20 14:54:11 +0530

    Routing issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import AddPerson from "./Components/gift/AddPerson";
 import Person from "./Components/gift/Person";
@@ -12,19 +12,17 @@ import EditGift from "./Components/gift/EditGift";
 function App() {
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/addperson" element={<AddPerson />} />
-        <Route exact path="/editperson" element={<EditPerson />} />
-
-        <Route exact path="/person" element={<Person />} />
-        <Route exact path="/gift" element={<GiftView />} />
-        <Route exact path="/addgift" element={<Gift />} />
-        <Route exact path="/editgift" element={<EditGift />} />
-
-
-
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/addperson" element={<AddPerson />} />
+          <Route exact path="/editperson" element={<EditPerson />} />
+          <Route exact path="/person" element={<Person />} />
+          <Route exact path="/gift" element={<GiftView />} />
+          <Route exact path="/addgift" element={<Gift />} />
+          <Route exact path="/editgift" element={<EditGift />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
